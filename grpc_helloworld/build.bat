4bbdb8ca915938f14f2dabab6c63133dd006a79e
Andrew Gray <github@xeisnet.com>
2018-10-18 22:40:26 +0800

    Finally got helloworld building and running
    - Pulled boringssl from the grpc build and arranged it so that cmake's
      find_package() can work with it.
    - Fixed grpc_helloworld\cmake_externalproject\CMakeLists.txt to point at
      the copied boringssl folder. It previously only looked for a
      system-installed OpenSSL, which I couldn't get to work correctly with
      any of the available OpenSSL Windows builds.
    - Copied protos from the grpc examples folder. Since the example itself is
      here, the protos should be also.
    - Fixed grpc_helloworld\CMakeLists.txt to reference the copied protos.
    - Copied zlib.dll from the grpc build into the helloworld output folder.
      We need Prime.

--- grpc_helloworld/build.bat
+++ grpc_helloworld/build.bat
@@ -2,10 +2,24 @@
 @setlocal
 pushd "%~dp0"
 
+rem Copy boringssl from the grpc build
+rmdir /q /s boringssl
+mkdir boringssl
+robocopy ../grpc/third_party/boringssl/include ./boringssl /e
+robocopy ../grpc/.build/third_party/boringssl/crypto/Release ./boringssl/lib crypto.lib
+robocopy ../grpc/.build/third_party/boringssl/ssl/Release ./boringssl/lib ssl.lib
+
 cd cmake_externalproject
-mkdir .build && cd .build
+mkdir .build
+cd .build
 cmake .. -G "Visual Studio 15 2017"
 cmake --build . --config Release
+
+rem Copy zlib from the grpc build
+robocopy ../../../grpc/.build/third_party/zlib/Release ./helloworld/Release zlib.dll
+
+rem If successful, built executables can be found in cmake_externalproject\.build\helloworld\Release
+dir .\helloworld\Release
 
 popd
 endlocal
